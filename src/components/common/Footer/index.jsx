6f39a3c5c02f17853d425d2ea6d6c4075a5b37e3
mikalaiFitkovich <mikalai.fi@plavno.io>
2025-11-24 14:47:16 +0100

    test for gh images url

--- src/components/common/Footer/index.jsx
+++ src/components/common/Footer/index.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 	return (
 		<footer className="footer">
 			<div className="footer__container">
-				<img src="/images/optimized/rocket.webp" alt="Rocket" />
+				<img src={`${process.env.PUBLIC_URL}/images/optimized/rocket.webp`} alt="Rocket" />
 				<p className="footer__text">Exciting space adventure!</p>
 			</div>
 		</footer>
