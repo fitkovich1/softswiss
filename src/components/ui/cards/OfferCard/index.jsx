082011ed8cff1dc7801d83d8a175653ef09990af
mikalaiFitkovich <mikalai.fi@plavno.io>
2025-11-24 14:53:44 +0100

    second test with gh images urls

--- src/components/ui/cards/OfferCard/index.jsx
+++ src/components/ui/cards/OfferCard/index.jsx
@@ -4,7 +4,7 @@ const OfferCard = ( { id, title, subTitle, bcgImage, ...props } ) => {
 	/*const navigate = useNavigate(); // To be used in the future, once a Router has been implemented */
 	return (
 		<div className="offers__cards-item" key={id} style={{
-			background: `linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(15, 20, 32, 0.79) 100%), url('/images/optimized/${bcgImage}') center/cover`
+			background: `linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(15, 20, 32, 0.79) 100%), url('${process.env.PUBLIC_URL}/images/optimized/${bcgImage}') center/cover`
 		}} {...props}>
 			<div className="offers__cards-item__text">
 				<h3 className="offers__cards-item__title">{title}</h3>
