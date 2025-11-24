ad97942d7cb9140d7ccfc4a198e1b0f37c25cbac
mikalaiFitkovich <mikalai.fi@plavno.io>
2025-11-24 17:40:02 +0100

    improving perfrmance

--- src/components/common/Header/HeaderNav/index.jsx
+++ src/components/common/Header/HeaderNav/index.jsx
@@ -8,8 +8,8 @@ const HeaderNav = ({ mobile = false }) => {
 			<ul className="header__menu">
 				{navMenu.map((item) => {
 					const { id, href, children, ariaLabel } = item;
-					return <li key={id} className="header__menu-item" aria-label={ariaLabel}>
-						<a href={href} className="header__menu-link">{children}</a>
+					return <li key={id} className="header__menu-item">
+						<a href={href} className="header__menu-link" aria-label={ariaLabel}>{children}</a>
 					</li>
 				})}
 			</ul>
